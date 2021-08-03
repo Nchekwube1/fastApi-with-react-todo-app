9148ffaf30f71dec1fff75d5d94b6b24ba310d1d
Nchekwube1 <unekwenchekwube@gmail.com>
2021-08-03 22:59:45 +0100

    addimg readme file

--- db.py
+++ db.py
@@ -16,14 +16,14 @@ async def fetchTodos():
     todos = []
     cursor = collection.find({})
     async for d in cursor:
-        todos.append(d)
+        todos.append(Todo(**d))
     return todos
 
 
 async def createTodo(todo):
     document = todo
     result = await collection.insert_one(document)
-    return "result"
+    return result
 
 
 async def updateTodo(id, todo):
